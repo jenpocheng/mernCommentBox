be447f9c0d7b77855e26cd4a68a08cd135b57bed
Jen-Po Cheng <jenpo@tumblr.com>
2016-11-17 20:22:31 -0500

    Create new route with GET and POST methods

--- server.js
+++ server.js
@@ -40,6 +40,31 @@ router.get('/', function(req, res) {
   res.json({ message: 'API Initialized!'});
 });
 
+//adding the /comments route to our /api router
+router.route('/comments')
+  //retrieve all comments from the database
+  .get(function(req, res) {
+    //looks at our Comment Schema
+    Comment.find(function(err, comments) {
+      if (err)
+        res.send(err);
+      //responds with a json object of our database comments.
+      res.json(comments)
+    });
+  })
+  //post new comment to the database
+  .post(function(req, res) {
+    var comment = new Comment();
+    (req.body.author) ? comment.author = req.body.author : null;
+    (req.body.text) ? comment.text = req.body.text : null;
+
+    comment.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Comment successfully added!' });
+    });
+  });
+
 //Use our router configuration when we call /api
 app.use('/api', router);
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
-import './index.css';
 
 ReactDOM.render(
   <CommentBox />,
